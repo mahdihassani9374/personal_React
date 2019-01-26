261f6fd4c0db435f64f7d19441816ac012810014
mahdihassani9374 <mahdihassani9374@gmail.com>
2019-01-26 11:46:55 +0330

    شیی هپشلث فخ لشممثقغ

--- src/Components/GalleryItem.js
+++ src/Components/GalleryItem.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import img from '../docs/images/profile.jpg';
+import img1 from '../docs/images/profile1.jpg';
+import img2 from '../docs/images/profile2.jpg';
 import ReactWOW from 'react-wow'
 
 
 export default class GalleryItem extends Component {
     state = {
-        gallery : [1,2,3,4,5,6,7,8]
+        gallery : [img,img1,img2]
     }
     render() {
         return (
@@ -15,7 +17,7 @@ export default class GalleryItem extends Component {
                     this.state.gallery.map((item,index)=>
                         <ReactWOW animation='zoomIn'  key={index}>
                               <div className="col-md-3 col-xs-6 col-sm-3">
-                                <img src={img} className="img-responsive img-thumbnail img-gallery" alt="مهدی و مبین حسنی " />
+                                <img src={item} className="img-responsive img-thumbnail img-gallery" alt="مهدی و مبین حسنی " />
                             </div>
                         </ReactWOW>                      
                     )
