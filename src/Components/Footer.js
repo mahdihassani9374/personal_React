c5099ac557f8fa1ab6d7fc75c8d2f790837d83c2
mahdihassani9374 <mahdihassani9374@gmail.com>
2019-04-03 13:45:38 +0430

    modify design

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,7 +5,7 @@ class Footer extends Component {
     render() {
         return (
             <footer className="footer">
-                <div className="row">
+                <div className="">
                     <div className="container">
                         <div className="footer__top">
                             <div className="">
